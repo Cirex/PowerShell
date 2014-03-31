a48d66391d5cc0638cdad053d72e8930eb3df878
Benjamin Bloch <cirex@aethernet.org>
2014-03-31 08:33:42 -0400

    Added git status to prompt

--- Library/Prompt.ps1
+++ Library/Prompt.ps1
@@ -1,10 +1,27 @@
 function Prompt
 {
+  if (Git-Repository)
+  {
+    $Branch = Git-Branch
+    $Status = Git-Status
+
+    $Title += "GIT [Branch: $Branch]"
+
+    $Status.Values | Foreach-Object { $Changes += $_ }
+    if ($Changes -gt 0)
+    {
+      $Title += ' (Uncommited Changes)'
+    }
+  }
+
+  Set-Title $Title
+
   $Path = Shorten-Path(Get-Location)
 
-  Write-Host '[' -NoNewLine -ForeGroundColor Blue
-  Write-Host $Path -NoNewLine -ForeGroundColor Blue
-  Write-Host ']' -ForeGroundColor Blue
+  Write-Host
+  Write-Host '[' -NoNewLine -ForeGroundColor Red
+  Write-Host $Path -NoNewLine -ForeGroundColor Yellow
+  Write-Host ']' -ForeGroundColor Red
 
   return '$ '
 }
