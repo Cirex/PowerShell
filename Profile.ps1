dab0644ff1b576719505baa6f9baf9b3a66a6ea0
Benjamin Bloch <cirex@fenix-novus.com>
2014-05-26 08:53:01 -0400

    Converted drive setup to storage spaces and using Atom now

--- Profile.ps1
+++ Profile.ps1
@@ -1,18 +1,18 @@
 ############################
 # Configuration
 ############################
-$BACKUP       = 'D:\Backup'
-$POWERSHELL   = 'D:\Backup\PowerShell'
-$ENVIRONMENT  = 'D:\Environment'
-$APPLICATIONS = 'D:\Applications'
-$PROJECTS     = 'D:\Projects'
+$APPLICATIONS = 'C:\Applications'
+$BACKUP       = 'C:\Backup'
+$POWERSHELL   = 'C:\Backup\PowerShell'
+$ENVIRONMENT  = 'C:\Development'
 
 $PERSISTENT_SESSION = 1
 $HISTORY_SIZE = 4096
 
-$ENV:BROWSER    = 'chrome'
-$ENV:EDITOR     = 'intype'
-$ENV:GIT_EDITOR = $ENV:EDITOR
+$ENV:BROWSER      = 'chrome'
+$ENV:MEDIA_PLAYER = "$APPLICATIONS\VLC\vlc.exe"
+$ENV:EDITOR       = "$APPLICATIONS\Atom\atom.exe"
+$ENV:GIT_EDITOR   = $ENV:EDITOR
 
 ############################
 # Vagrant
@@ -23,25 +23,21 @@ $ENV:VAGRANT_DEFAULT_PROVIDER = 'hyperv'
 # Git
 ############################
 $ENV:GIT_AUTHOR_NAME     = 'Benjamin Bloch'
-$ENV:GIT_AUTHOR_EMAIL    = 'cirex@aethernet.org'
+$ENV:GIT_AUTHOR_EMAIL    = 'cirex@fenix-novus.com'
 $ENV:GIT_COMMITTER_NAME  = $ENV:GIT_AUTHOR_NAME
 $ENV:GIT_COMMITTER_EMAIL = $ENV:GIT_AUTHOR_EMAIL
-
-############################
-# GitHub
-############################
-$GITHUB_API_TOKEN = 'GITHUB_API_TOKEN'
-$GITHUB_USER_NAME = 'Cirex'
 
 set-variable -Name HOME -Value ($BACKUP) -Force
 (get-psprovider FileSystem).Home = $BACKUP
 
 . $POWERSHELL\Library\Aliases.ps1
 . $POWERSHELL\Library\Backup.ps1
+. $POWERSHELL\Library\Compression.ps1
+. $POWERSHELL\Library\FFmpeg.ps1
 . $POWERSHELL\Library\Functions.ps1
 . $POWERSHELL\Library\Git.ps1
-. $POWERSHELL\Library\GitHub.ps1
 . $POWERSHELL\Library\History.ps1
+. $POWERSHELL\Library\LiveStreamer.ps1
 . $POWERSHELL\Library\Paths.ps1
 . $POWERSHELL\Library\Prompt.ps1
 . $POWERSHELL\Library\VisualStudio.ps1
@@ -49,15 +45,21 @@ set-variable -Name HOME -Value ($BACKUP) -Force
 Set-VisualStudio 2013
 
 Append-Path $ENVIRONMENT\Android
+Append-Path $ENVIRONMENT\AutoIt
+Append-Path $ENVIRONMENT\EncFS
 Append-Path $ENVIRONMENT\FFmpeg\Bin
-Append-Path $ENVIRONMENT\Gems\Bin
 Append-Path $ENVIRONMENT\Git\Cmd
 Append-Path $ENVIRONMENT\ImageMagick
+Append-Path $ENVIRONMENT\LiveStreamer
 Append-Path $ENVIRONMENT\MongoDB
+Append-Path $ENVIRONMENT\Nginx
 Append-Path $ENVIRONMENT\NodeJS
+Append-Path $ENVIRONMENT\Python
+Append-Path $ENVIRONMENT\Python\Scripts
 Append-Path $ENVIRONMENT\Redis
 Append-Path $ENVIRONMENT\Ruby\Bin
-Append-Path $ENVIRONMENT\Tools
+Append-Path $ENVIRONMENT\Ruby\Gems\Bin
+Append-Path $ENVIRONMENT\Utilities
 
 Append-Path-Extension 'rb'
 Append-Path-Extension 'rbw'
@@ -65,7 +67,7 @@ Append-Path-Extension 'py'
 Append-Path-Extension 'pyw'
 
 $ENV:HOME        = $POWERSHELL
-$ENV:GEM_HOME    = $ENVIRONMENT + '\Gems'
+$ENV:GEM_HOME    = $ENVIRONMENT + '\Ruby\Gems'
 $ENV:MAGICK_HOME = $ENVIRONMENT + '\ImageMagick'
 $ENV:NODE_PATH   = $ENVIRONMENT + '\NodeJS\Node_Modules'
 
